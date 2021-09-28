591ade45413e6342d5eadc6935547198432de1ca
Marcus Pöhls <marcus.poehls@gmail.com>
2021-09-28 06:05:09 +0200

    prepare findLastIndex method

--- test/set.js
+++ test/set.js
@@ -506,6 +506,22 @@ test('handle objects', () => {
   expect(set.size()).toEqual(2)
 })
 
+test('findLastIndex', () => {
+  const ids = Set.from(1, 2, 3, 4)
+  expect(ids.findLastIndex(id => id < 4)).toBe(2)
+  expect(ids.findLastIndex(id => id > 3)).toBe(3)
+  expect(ids.findLastIndex(id => id > 10)).toBe(-1)
+
+  const set = Set.from([
+    { id: 1, name: 'Marcus', subscribed: true },
+    { id: 2, name: 'Norman', subscribed: true },
+    { id: 3, name: 'Christian', subscribed: true }
+  ])
+
+  expect(set.findLastIndex((value) => value.subscribed)).toEqual(2)
+  expect(set.findLastIndex((value) => value.name === 'Supercharge')).toBe(-1)
+})
+
 test.run()
 
 class User {
